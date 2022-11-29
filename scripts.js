41f611b24bbe506f1308d3baf02d6a41f3a2174a
Adir <slutzkis@gmail.com>
2022-11-29 10:59:52 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,6 +18,6 @@ function gridSizeV(event) {
   }
 
   function removeItem(event){
-    event.target.parentElement.style.transform = "scaleY(0)";
+    event.target.parentElement.style.height = "0";
     // event.target.parentElement.outerHTML = "";
   }
