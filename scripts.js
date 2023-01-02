cc4b8df1a9f9f4b48d521e12a9d3891274c33325
Adir <slutzkis@gmail.com>
2023-01-02 11:04:49 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,11 +9,10 @@ function gridSizeV(event) {
   function addtoGrid() {
     document.getElementById("wrapper").innerHTML +=
       '<div class="grid-item grid-new"><div class="action-icon pin" tooltip="Pin to top" onclick="pinItem(event);"></div><div class="action-icon close-x" tooltip="Delete item" onclick="removeItem(event);"></div><div class="icon-corner"><input type="color" tooltip="Change color" class="action-icon color-picker" onchange="changeColor(event);" value="#ffffff" /><div class="action-icon arrow-v" tooltip="Change item height" onclick="gridSizeV(event);"></div><div class="action-icon arrow-h" tooltip="Change item width" onclick="gridSize(event);"></div></div><div class="content"></div></div>';
-      removeNew();
+      setInterval(function(){removeNew();}, 300);
   }
 
   function removeNew(){
-    setInterval(function(){
       var x = document.getElementsByClassName("grid-item");
       var i;
       for (i = 0; i < x.length; i++) {
@@ -21,7 +20,6 @@ function gridSizeV(event) {
           x[i].classList.remove("grid-new");
         }
       }
-    }, 300);
   }
   
   function changeColor(event){
