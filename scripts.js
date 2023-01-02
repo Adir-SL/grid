7c7d86bf2728d614ffc0e08f71e706f5401207e3
Adir <slutzkis@gmail.com>
2023-01-02 11:07:52 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ function gridSizeV(event) {
       var elemDiv = document.createElement('div');
       elemDiv.className = 'grid-item';
       elemDiv.innerHTML = '<div class="action-icon pin" tooltip="Pin to top" onclick="pinItem(event);"></div><div class="action-icon close-x" tooltip="Delete item" onclick="removeItem(event);"></div><div class="icon-corner"><input type="color" tooltip="Change color" class="action-icon color-picker" onchange="changeColor(event);" value="#ffffff" /><div class="action-icon arrow-v" tooltip="Change item height" onclick="gridSizeV(event);"></div><div class="action-icon arrow-h" tooltip="Change item width" onclick="gridSize(event);"></div></div><div class="content"></div>';
-      document.body.appendChild(elemDiv);
+      document.getElementById("wrapper").appendChild(elemDiv);
   }
 
   function removeNew(){
