d38987ef31c4bfb14d8d8dc196590557e6d3bfcd
Adir <slutzkis@gmail.com>
2023-01-02 10:56:54 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,7 +11,11 @@ function gridSizeV(event) {
       '<div class="grid-item grid-new" onload="removeNew(event);"><div class="action-icon pin" tooltip="Pin to top" onclick="pinItem(event);"></div><div class="action-icon close-x" tooltip="Delete item" onclick="removeItem(event);"></div><div class="icon-corner"><input type="color" tooltip="Change color" class="action-icon color-picker" onchange="changeColor(event);" value="#ffffff" /><div class="action-icon arrow-v" tooltip="Change item height" onclick="gridSizeV(event);"></div><div class="action-icon arrow-h" tooltip="Change item width" onclick="gridSize(event);"></div></div><div class="content"></div></div>';
   }
 
-  function removeNew(event){}
+  function removeNew(event){
+    setInterval(function(){
+      event.target.classList.remove("grid-new");
+    }, 1000);
+  }
   
   function changeColor(event){
     console.log(event.target.value);
