df7f862dfb2b4c3a2f062c7e0e6e41373ea52b5e
Adir <slutzkis@gmail.com>
2023-01-05 16:40:08 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,14 +16,13 @@ function gridSizeV(event) {
   function changeColor(event){
     console.log(event.target.value);
     event.target.parentElement.parentElement.style.backgroundColor = event.target.value;
-    
-    event.target.outerHTML = '<input type="color" tooltip="Change color" class="action-icon color-picker" onchange="changeColor(event);" value="' + event.target.value + '">';
 
     colorContrast = hexToRgb(event.target.value).r + hexToRgb(event.target.value).g + hexToRgb(event.target.value).b;
-    console.log(event.target.parentElement)
+    console.log(event.target);
     if(colorContrast < 255){
       event.target.parentElement.parentElement.style.color = "#ffffff";
     }
+    event.target.outerHTML = '<input type="color" tooltip="Change color" class="action-icon color-picker" onchange="changeColor(event);" value="' + event.target.value + '">';
   }
 
   function removeItem(event){
