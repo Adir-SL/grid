5808e5df8c811bd0a0d77d438868cc18cb20ef38
Adir <slutzkis@gmail.com>
2023-01-30 18:13:58 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -28,10 +28,10 @@ function gridSizeV(event) {
         scrollFunc();
       }else{
         var elemDiv = document.createElement('div');
-      elemDiv.className = 'grid-item grid-new';
-      elemDiv.innerHTML = '<div class="action-icon pin" tooltip="Pin to top" onclick="pinItem(event);"></div><div class="action-icon close-x" tooltip="Delete item" onclick="removeItem(event);"></div><div class="icon-corner"><input type="color" tooltip="Change color" class="action-icon color-picker" onchange="changeColor(event);" onclick="changeAllColors();" value="#ffffff" /><div class="action-icon arrow-v" tooltip="Change item height" onclick="gridSizeV(event);"></div><div class="action-icon arrow-h" tooltip="Change item width" onclick="gridSize(event);"></div></div><div class="content"></div>';
-      document.getElementById("wrapper").appendChild(elemDiv);
-      window.scrollTo(0,document.body.scrollHeight);
+        elemDiv.className = 'grid-item grid-new';
+        elemDiv.innerHTML = '<div class="action-icon pin" tooltip="Pin to top" onclick="pinItem(event);"></div><div class="action-icon close-x" tooltip="Delete item" onclick="removeItem(event);"></div><div class="icon-corner"><input type="color" tooltip="Change color" class="action-icon color-picker" onchange="changeColor(event);" onclick="changeAllColors();" value="#ffffff" /><div class="action-icon arrow-v" tooltip="Change item height" onclick="gridSizeV(event);"></div><div class="action-icon arrow-h" tooltip="Change item width" onclick="gridSize(event);"></div></div><div class="content"></div>';
+        document.getElementById("wrapper").appendChild(elemDiv);
+        window.scrollTo(0,document.body.scrollHeight);
       }
   }, 10);
   }
